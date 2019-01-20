9fd7d89fd1afc0929c3dbe9f836adf402e3b9f63
wangyao <yaowang@travelsky.com>
2019-01-20 14:52:37 +0800

    路由组件实现懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,57 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import recommend from 'components/recommend/recommend'
-import rank from 'components/rank/rank'
-import search from 'components/search/search'
-import singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import User from 'components/user-center/user-center'
 /* 子路由使用步骤：导入组件 配置子路由 <router-view>标签 事件中push进去 */
-import Disc from 'components/disc/disc'
-import TopList from 'components/topList/topList'
 
 Vue.use(Router)
 
+/* 路由懒加载的使用 */
+const recommend = ((resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+})
+
+const rank = ((resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+})
+
+const search = ((resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+})
+
+const singer = ((resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+})
+
+const SingerDetail = ((resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+})
+
+const User = ((resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+})
+
+const Disc = ((resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+})
+
+const TopList = ((resolve) => {
+  import('components/topList/topList').then((module) => {
+    resolve(module)
+  })
+})
 export default new Router({
   routes: [
     {
